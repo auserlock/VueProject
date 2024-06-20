da156c10d31bf623f15aa2d01a0b6deff57f2fee
wjg <arch_domartic@outlook.com>
2024-06-20 18:34:05 +0800

    更新 pinia仓库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+</script>
 
 <template>
-  <div>我是APP</div>
+  <div>
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('asd')">登录</el-button>
+    <el-button @click="userStore.removeToken()">退出</el-button>
+  </div>
 </template>
 
 <style scoped></style>
