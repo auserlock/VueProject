55cab1e332c9a35cd9d220c3340b602ef16b1164
wjg <arch_domartic@outlook.com>
2024-06-21 09:26:09 +0800

    注册校验

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,30 +1,88 @@
 <script setup>
 import { User, Lock } from '@element-plus/icons-vue'
 import { ref } from 'vue'
-const isRegister = ref(true)
+const isRegister = ref(false)
+const form = ref()
+
+const formModel = ref({
+  userId: '',
+  password: '',
+  repassword: ''
+})
+const rules = {
+  userId: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 6, message: '用户名包含至少6位字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码必须是6-16位非空字符',
+      trigger: 'blur'
+    }
+  ],
+  repassword: [
+    { required: true, message: '请再次输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码必须是6-16位非空字符',
+      trigger: 'blur'
+    },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== formModel.value.password) {
+          callback(new Error('输入密码不一致'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+const register = async () => {
+  await form.value.validate()
+  console.log('开始注册')
+}
 </script>
 
 <template>
   <el-row class="login-page">
     <el-col :span="12" class="bg"></el-col>
     <el-col :span="6" :offset="3" class="form">
-      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+      <el-form
+        :model="formModel"
+        :rules="rules"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
         <el-form-item>
           <h1>注册</h1>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item>
+        <el-form-item prop="userId">
           <el-input
+            v-model="formModel.userId"
+            maxlength="16"
+            show-word-limit
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formModel.password"
             :prefix-icon="Lock"
             type="password"
             placeholder="请输入密码"
             show-password
           />
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="repassword">
           <el-input
+            v-model="formModel.repassword"
             :prefix-icon="Lock"
             type="password"
             placeholder="请再次输入密码"
@@ -32,7 +90,12 @@ const isRegister = ref(true)
           />
         </el-form-item>
         <el-form-item>
-          <el-button class="button" type="primary" auto-insert-space>
+          <el-button
+            @click="register"
+            class="button"
+            type="primary"
+            auto-insert-space
+          >
             注册
           </el-button>
         </el-form-item>
@@ -81,17 +144,19 @@ const isRegister = ref(true)
 <style lang="scss" scoped>
 .login-page {
   height: 100vh;
+  // background: url('@/assets/background.png') no-repeat center / cover;
+  border-radius: 0 20px 20px 20px;
   background-color: #fff;
   .bg {
-    background: url('@/assets/ad.jpeg') no-repeat 60% center / 400px auto;
-    //   url('@/assets/login_bg.jpg') no-repeat center / cover;
-    border-radius: 0 20px 20px 0;
+    // background: url('@/assets/name.png') no-repeat center center / 500px auto;
+    background-color: #f5f1f3;
   }
   .form {
     display: flex;
     flex-direction: column;
     justify-content: center;
     user-select: none;
+    // background: url('@/assets/ad.jpeg') no-repeat center 90% / 300px auto;
     .title {
       margin: 0 auto;
     }
